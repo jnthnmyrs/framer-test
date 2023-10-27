0bf9bb75f3f4b2ade16445dba7a34ff343f2ff01
jnthnmyrs <jonathan.andrew.myers@gmail.com>
2023-10-27 00:49:59 -0600

    this is getting good

--- components/MotionTestDiv.tsx
+++ components/MotionTestDiv.tsx
@@ -1,13 +1,23 @@
 "use client"
 
 import Image from "next/image"
+import Script from "next/script"
 import { useRouter } from "next/navigation"
 import { toast } from "react-hot-toast"
 import { motion } from "framer-motion"
 import { useState } from "react"
 import Link from "next/link"
 
+
+declare global {
+    interface Window {
+        onYouTubeIframeAPIReady: () => void
+    }
+}
+
 const MotionTestDiv = () => {
+
+
 
     const router = useRouter()
 
@@ -20,6 +30,8 @@ const MotionTestDiv = () => {
     const [xValue, setXValue] = useState(0)
     const [yValue, setYValue] = useState(0)
     const [score, setScore] = useState(0)
+
+    const [player, setPlayer] = useState<any>(null)
 
 
     const squareClasses = () => {
@@ -75,6 +87,49 @@ const MotionTestDiv = () => {
                 }
                     break;
             }
+
+
+            const tag = document.createElement("script")
+            tag.src = "https://www.youtube.com/iframe_api"
+            const firstScriptTag = document.getElementsByTagName("script")[0]
+            firstScriptTag.parentNode?.insertBefore(tag, firstScriptTag)
+      
+            window.onYouTubeIframeAPIReady = () => {
+                // @ts-expect-error
+              const newPlayer = new window.YT.Player("player", {
+                height: "100%",
+                width: "100%",
+                videoId: "jzj7STruKgQ",
+                playerVars: {
+                    autoplay: 1,
+                    controls: 1,
+                    // disablekb: 1,
+                    // enablejsapi: 1,
+                    fs: 1,
+                    // loop: 1,
+                    modestbranding: 1,
+                    // playsinline: 1,
+                    // rel: 0,
+                    showinfo: 0,
+                },
+                events: {
+                  onStateChange: (event: any) => {
+                    // @ts-expect-error
+                    if (event.data === window.YT.PlayerState.ENDED) {
+                      console.log("Video finished playing")
+                        newPlayer.destroy()
+                        setScore(0)
+                        launchToast("Back to the game! 👋")
+                    }
+                  }
+                }
+              })
+              setPlayer(newPlayer)
+            }
+
+
+
+
         }
     }
 
@@ -88,59 +143,61 @@ const MotionTestDiv = () => {
                 score == 0
                     ? <>
                         <div className="flex flex-col justify-between ">
-                <div className="grid mx-auto justify-center grid-cols-1 md:grid-cols-2 gap-16">
-                    <motion.div
-                        onClick={makeItDoStuff}
-                        className={squareClasses()}
-                        initial={{
-                            x: `${Math.random() * 90 - 45}vw`,
-                            y: `${Math.random() * 90 - 45}vh`,
-                            opacity: 1,
-                            scale: 0.5,
-                            rotate: 0,
-                        }}
-                        animate={{
-                            x: `${xValue}vw`,
-                            y: `${yValue}vh`,
-                            rotate: stuff.rotation,
-                            scale: stuff.scale,
-                            opacity: stuff.opacity,
-                        }}
-                        transition={{ duration: 0.5, type: "spring" }}
-                        whileTap={{ scale: 0.8 }}
-                    />
-                    <div className="grid grid-cols-1 gap-8 md:gap-2 pointer-events-none user-select-none">
+                            <div className="grid mx-auto justify-center grid-cols-1 md:grid-cols-2 gap-16">
+                                <motion.div
+                                    onClick={makeItDoStuff}
+                                    className={squareClasses()}
+                                    initial={{
+                                        x: `${Math.random() * 90 - 45}vw`,
+                                        y: `${Math.random() * 90 - 45}vh`,
+                                        opacity: 1,
+                                        scale: 0.5,
+                                        rotate: 0,
+                                    }}
+                                    animate={{
+                                        x: `${xValue}vw`,
+                                        y: `${yValue}vh`,
+                                        rotate: stuff.rotation,
+                                        scale: stuff.scale,
+                                        opacity: stuff.opacity,
+                                    }}
+                                    transition={{ duration: 0.5, type: "spring" }}
+                                    whileTap={{ scale: 0.8 }}
+                                />
+                                <div className="grid grid-cols-1 gap-8 md:gap-2 pointer-events-none user-select-none">
 
-                        <p className="text-white z-50 text-4xl pointer-events-none user-select-none"><span className="text-white/50">ROTATION:</span> {stuff.rotation}°</p>
-                        <p className="text-white z-50 text-4xl pointer-events-none user-select-none"><span className="text-white/50">X:</span> {Math.round(xValue)}</p>
-                        <p className="text-white z-50 text-4xl pointer-events-none user-select-none"><span className="text-white/50">Y:</span> {Math.round(yValue)}</p>
-                        <p className="text-white z-50 text-4xl pointer-events-none user-select-none"><span className="text-white/50">SCORE:</span> {score} </p>
-                        {/* <button onClick={secretReset} className=" text-stone-700">secret reset</button> */}
+                                    <p className="text-white z-50 text-4xl pointer-events-none user-select-none"><span className="text-white/50">ROTATION:</span> {stuff.rotation}°</p>
+                                    <p className="text-white z-50 text-4xl pointer-events-none user-select-none"><span className="text-white/50">X:</span> {Math.round(xValue)}</p>
+                                    <p className="text-white z-50 text-4xl pointer-events-none user-select-none"><span className="text-white/50">Y:</span> {Math.round(yValue)}</p>
+                                    <p className="text-white z-50 text-4xl pointer-events-none user-select-none"><span className="text-white/50">SCORE:</span> {score} </p>
+                                    {/* <button onClick={secretReset} className=" text-stone-700">secret reset</button> */}
 
-                    </div>
+                                </div>
 
-                    
-                </div>
-                {/* <div className="relative flex justify-end">
+
+                            </div>
+                            {/* <div className="relative flex justify-end">
                     <div className=" text-white flex flex-col items-center justify-center">
                         <Link href="https://jonathanandrewmyers.com" target="_blank">
                             <Image src="/jamsig.png?v=2" alt="made by jam" width={40} height={40} />
                         </Link>
                     </div>
                 </div> */}
-            </div>
+                        </div>
                     </>
                     : <>
                         <div className="absolute top-0 left-0 h-screen w-screen">
 
-                        <iframe width="100%" height="100%" src="https://www.youtube.com/embed/jzj7STruKgQ?autoplay=1" title="YouTube video player" 
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                            <div id="player"></div>
+
+                            {/* <iframe width="100%" height="100%" src="https://www.youtube.com/embed/jzj7STruKgQ?autoplay=1" title="YouTube video player"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe> */}
 
                         </div>
                     </>
             }
 
-            
+
         </>
     )
 }
